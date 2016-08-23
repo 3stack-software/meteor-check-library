3c8a5ee867f38ee8102b053b665ddc9f3749a679
Nathan Muir <ndmuir@gmail.com>
2016-08-23 18:06:55 +1000

    Bump to 1.0.5

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: '3stack:match-library',
-  version: '1.0.4',
+  version: '1.0.5',
   summary: 'A library of additional check/match filters',
   git: 'https://github.com/3stack-software/meteor-match-library',
   documentation: 'README.md'
